2543cd463ea0943440ec18bbc5dda9fee45fa371
Fakhrial <dakarafakhrial@gmail.com>
2025-06-09 19:11:28 +0700

    first commit

---
+++ app/Http/Controllers/FrontController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Shoe;
+use App\Services\FrontService;
+use Illuminate\Http\Request;
+
+class FrontController extends Controller
+{
+    protected $frontService;
+
+    public function __construct(FrontService $frontService)
+    {
+        $this->frontService = $frontService;
+    }
+
+    public function index()
+    {
+        $data = $this->frontService->getFrontPageData();
+        //dd($data);
+        return view('front.index', $data);
+    }
+
+    public function details(Shoe $shoe){
+        return view('front.details', compact('shoe'));
+    }
+
+    public function category(Category $category){
+        return view('front.category', compact('category'));
+    }
+}
---
+++ app/Repositories/ShoeRepository.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Shoe;
+use App\Repositories\Contracts\ShoeRepositoryInterface;
+
+class ShoeRepository implements ShoeRepositoryInterface
+
+{
+    public function getPopularShoes($limit = 4)
+    {
+        return Shoe::where('is_popular', true)->take($limit)->get();
+    }
+
+    public function searchByName(string $keyword)
+    {
+        return Shoe::where('name', 'LIKE', '%' . $keyword .  '%')->get();
+    }
+
+    public function getAllNewShoes()
+    {
+        return Shoe::latest()->get();
+    }
+
+    public function find($Id)
+    {
+        return Shoe::find($Id);
+    }
+
+    public function getPrice($shoeId)
+    {
+        $shoe = $this->find($shoeId);
+        return $shoe ? $shoe->$price : 0;
+    }
+}
